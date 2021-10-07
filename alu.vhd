298a2fb85950f23d05975ed9acf6c0d23144a6fe
Zacharie McCormick <zacfire1998@hotmail.fr>
2021-10-07 20:45:37 +0200

    Added the enable signal to the alu.

--- alu.vhd
+++ alu.vhd
@@ -28,30 +28,33 @@ architecture behave of alu is
         return res_v;
       end function;
 
+      signal sum_tmp : std_logic_vector(N-1 downto 0);
+
 begin
     process(A, B, Op)
     begin
         case Op is
-            when "000" => Sum <= std_logic_vector(signed(A)+signed(B));
-            when "001" => Sum <= std_logic_vector(signed(A)-signed(B));
-            when "010" => Sum <= A AND B;
-            when "011" => Sum <= A OR B;
-            when "100" => Sum <= A XOR B;
-            when "101" => Sum <= NOT A;
-            when "110" => Sum <= A;
-            when "111" => Sum <= A + 1;
-            when others => Sum <= (others => '0');
+            when "000" => sum_tmp <= std_logic_vector(signed(A)+signed(B));
+            when "001" => sum_tmp <= std_logic_vector(signed(A)-signed(B));
+            when "010" => sum_tmp <= A AND B;
+            when "011" => sum_tmp <= A OR B;
+            when "100" => sum_tmp <= A XOR B;
+            when "101" => sum_tmp <= NOT A;
+            when "110" => sum_tmp <= A;
+            when "111" => sum_tmp <= A + 1;
+            when others => sum_tmp <= (others => '0');
         end case;
     end process;
-    Z_Flag <= and_reduct(NOT Sum);
-    N_Flag <= Sum(N-1);
+    Sum <= sum_tmp when (en = '1') else (others => '0');
+    Z_Flag <= and_reduct(NOT Sum) when (en = '1') else '0';
+    N_Flag <= Sum(N-1) when (en = '1') else '0';
     -- Overflow occurs when either adding two positive and getting a negative (001),
     -- Adding two negative and getting a positive (110),
     -- Subtracting a negative from a positive and getting a negative (011),
     -- Subtracting a positive from a negative and getting a positive (100)
-    O_Flag <= '1' when ((Op = "000") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "001")) else
-              '1' when ((Op = "000") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "110")) else
-              '1' when ((Op = "001") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "011")) else
-              '1' when ((Op = "001") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "100")) else
+    O_Flag <= '1' when ((en = '1') AND (Op = "000") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "001")) else
+              '1' when ((en = '1') AND (Op = "000") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "110")) else
+              '1' when ((en = '1') AND (Op = "001") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "011")) else
+              '1' when ((en = '1') AND (Op = "001") AND (std_logic_vector'(A(N-1) & B(N-1) & Sum(N-1)) = "100")) else
               '0';
 end architecture;
